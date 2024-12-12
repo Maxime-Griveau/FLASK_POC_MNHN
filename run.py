3b43491fafb8065cf3943afd16c2addfa5894018
Maxime <griveaumaxime@gmail.com>
2024-12-12 14:22:13 +0100

    test V2

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from app.app import app
+import os
+from app import app  # Importez votre application Flask depuis `app.py`
 
 if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"])
-    
+    # Récupérer le port depuis les variables d'environnement
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
