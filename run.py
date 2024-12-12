0e6cf02017141a851cb044e2122d3ee4636f3bae
Maxime <griveaumaxime@gmail.com>
2024-12-12 15:11:57 +0100

    premier commit

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-from app import app  # Importez votre application Flask depuis `app.py`
-
-if __name__ == "__main__":
-    # Récupérer le port depuis les variables d'environnement
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
